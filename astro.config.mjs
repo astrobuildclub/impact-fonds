523c7f9feddeb78abd548087085539a4b6ae3670
mieras <mieras@gmail.com>
2024-06-13 12:22:37 +0200

    layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind({ applyBaseStyles: false,})]
+  integrations: [mdx(), tailwind({ applyBaseStyles: false })],
 });
