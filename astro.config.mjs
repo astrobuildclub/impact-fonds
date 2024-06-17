bdd9320980d781af985081dfb71b90887f4b8ac5
mieras <mieras@gmail.com>
2024-06-17 15:53:29 +0200

    merge maarten

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import tailwind from '@astrojs/tailwind';
+import icons from 'astro-icon'; // Zorg ervoor dat je dit correct importeert als het bestaat
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind({ applyBaseStyles: false })],
+  integrations: [icons(), mdx(), tailwind({ applyBaseStyles: false })],
 });
